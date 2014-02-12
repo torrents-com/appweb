e387adc76f7ce24e764365cdad89a2db06054e1d
leo <leo@mp2p.net>
2014-02-12 14:25:51 +0100

    Analytics code moved to configuration file.

--- appweb/web.py
+++ appweb/web.py
@@ -237,15 +237,7 @@ def create_app(config=None, debug=False):
 def init_g(app):
     g.license_name = "torrents"
 
-    g.analytics_code = """<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-38333996-2']);  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>"""
+    g.analytics_code = app.config["ANALYTICS_CODE"]
 
     # caracteristicas del cliente
     g.search_bot = is_search_bot()
